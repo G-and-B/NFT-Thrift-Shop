896dfd52c94df342808250ddc30a9e858cd882df
Gianfranco Dutka <Gianfranco.Dutka@Citrix.com>
2022-04-28 13:44:41 -0400

    started working on the landing page

---
+++ components/Footer.js
@@ -0,0 +1,15 @@
+
+const footerStyle = {
+    backgroundColor: "blue",
+    color: "white",
+    width: "100%",
+    height: "50px"
+  };
+
+  const Footer = () =>(
+    <div className="Footer" style={footerStyle} >
+        Footer
+    </div>
+  );
+
+  export default Footer;
---
+++ components/Navbar.js
@@ -0,0 +1,15 @@
+
+const navbarStyle = {
+    backgroundColor: "blue",
+    color: "white",
+    width: "100%",
+    height: "50px"
+  };
+
+  const Navbar = () =>(
+    <div className="Navbar" style={navbarStyle} >
+        Navbar
+    </div>
+  );
+
+  export default Navbar;
