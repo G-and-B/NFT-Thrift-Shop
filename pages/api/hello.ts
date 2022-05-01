988f1df3647477963287975a3c7168e950033be3
Gianfranco <gdutk007@gmail.com>
2022-04-30 20:42:48 -0400

    undid changes

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,20 +1,17 @@
 // // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
-}
-
-
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-// import type { NextApiRequest, NextApiResponse } from 'next'
-
-// type Data = {
-//   name: string
-// }
-
-// export default function handler(
-//   req: NextApiRequest,
-//   res: NextApiResponse<Data>
-// ) {
+// export default function handler(req, res) {
 //   res.status(200).json({ name: 'John Doe' })
 // }
+//Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+  name: string
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  res.status(200).json({ name: 'John Doe' })
+}
