aa945739c879d33d30c0242b6432c463d1619cdc
Gianfranco Dutka <Gianfranco.Dutka@Citrix.com>
2022-04-30 21:28:48 -0400

    test

--- pages/Create.tsx
+++ pages/Create.tsx
@@ -5,20 +5,58 @@ import styles from '../styles/Home.module.css'
 
 export default function Home(){
   return (
-    <>
-     This is the create page...
-    <ul>
-      <li>
-        <Link href="/">
-          <a>Home</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/yourCollection">
-          <a>Your Collection</a>
-        </Link>
-      </li>
-    </ul>
-    </>
+<div className="object-center">
+      <button 
+      // id="walletButton" 
+      // onClick={connectWalletPressed}
+      >
+        {/* {walletAddress.length > 0 ? (
+          "Connected: " +
+          String(walletAddress).substring(0, 6) +
+          "..." +
+          String(walletAddress).substring(38)
+        ) : (
+          <span>Connect Wallet</span>
+        )} */}
+      </button>
+      <br></br>
+      <h1 
+      // id="title"
+      >🧙‍♂️ 
+      Alchemy NFT Minter
+      </h1>
+      <p>
+        Simply add your asset's link, name, and description, then press "Mint."
+      </p>
+      <br/>
+      <form>
+        <h2>🖼 Link to asset: </h2>
+        <input
+          type="text"
+          placeholder="e.g. https://gateway.pinata.cloud/ipfs/<hash>"
+          // onChange={(event) => setURL(event.target.value)}
+        />
+        <h2>🤔 Name: </h2>
+        <input
+          type="text"
+          placeholder="e.g. My first NFT!"
+          // onChange={(event) => setName(event.target.value)}
+        />
+        <h2>✍️ Description: </h2>
+        <input
+          type="text"
+          placeholder="e.g. Even cooler than cryptokitties ;)"
+          // onChange={(event) => setDescription(event.target.value)}
+        />
+      </form>
+      <button 
+        // id="mintButton" onClick={onMintPressed}
+        >
+        Mint NFT
+      </button>
+      {/* <p id="status">
+        {status}
+      </p> */}
+    </div>
   )
 }
