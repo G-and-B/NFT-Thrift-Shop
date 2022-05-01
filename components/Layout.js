927296d8774ad3d93d6a032385e017cf4b82e746
Gianfranco <gdutk007@gmail.com>
2022-05-01 13:06:22 -0400

    testing

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,17 @@
 
 import Navbar from './Navbar';
 import Footer from './Footer';
+import layoutStyle from '../styles/Layout.module.css'
 
 export default function Layout({children}){
     return (
         <>
-        <div className="flex flex-col h-screen justify-between">
-        <Navbar/>
-        <main>{children}</main>
-        <Footer > </Footer>
+        <div 
+        // className="flex flex-col h-screen justify-between"
+        >
+        <Navbar className={layoutStyle.Navbar}/>
+        <main className={layoutStyle.main} >{children}</main>
+        <Footer className={layoutStyle.Footer} > </Footer>
         </div>
         </>
     )
